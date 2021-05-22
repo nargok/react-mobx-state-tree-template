f2ad12da9d47d1e5d8b1337d66da54785b44211f
nargok <nargok@gmail.com>
2021-05-22 13:38:03 +0900

    use store

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { initialStore, Provider } from './store/'
+
+const store = initialStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={store}>
+      <div className="App">
+      </div>
+    </Provider>
   );
 }
 
